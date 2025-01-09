a6995dd3d0b9ae6ab6ae18ca57a0e702048c9da8
Muhammad Ibad <mibad0338@gmail.com>
2025-01-09 01:58:35 +0100

    Update Key.js

--- Key.js
+++ Key.js
@@ -1,12 +1,5 @@
 export default {
     firebaseConfig : {
-        apiKey: "AIzaSyCYVKdgbJBo2lJH6hhnC8NJWmxlSx4oB3M",
-        authDomain: "olx-clone-18e6b.firebaseapp.com",
-        databaseURL: "https://olx-clone-18e6b.firebaseio.com",
-        projectId: "olx-clone-18e6b",
-        storageBucket: "olx-clone-18e6b.appspot.com",
-        messagingSenderId: "351773459050",
-        appId: "1:351773459050:web:b496313c75ce87d245c013",
-        measurementId: "G-1270JK9YKE"
+       
     }
 }
